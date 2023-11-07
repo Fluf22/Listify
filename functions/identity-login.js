413e0bc3c5b6c9cb4db5bd44a92915c7045d0c5c
Thomas Raffray <Fluf22@users.noreply.github.com>
2023-11-07 12:06:36 +0100

    Add identity login and validation functions
    The commit adds two new files, `identity-login.js` and `identity-validate.js`, which contain functions for handling login and validation events in an identity service. These functions parse the request body, check if the event is a login or validation event, and authenticate the user based on their email. If the email is unauthorized, the functions return a 401 status code and a JSON message indicating unauthorized access. Otherwise, they return a 200 status code. The functions also log relevant information for debugging purposes.

---
+++ functions/identity-login.js
@@ -0,0 +1,33 @@
+const createUserRoute = require('./users/create');
+
+const handler = async (event, context) => {
+    const reqBody = JSON.parse(event.body);
+    console.log("Req body: ", reqBody);
+    if (reqBody.event === "login") {
+        const authorizedEmails = [];
+        try {
+            authorizedEmails.push(...JSON.parse(process.env.AUTHORIZED_EMAILS));
+        } catch (e) {
+            console.error("failed to parse authorized emails: ", e);
+        }
+        if (authorizedEmails.includes(reqBody.user.email) === false) {
+            return {
+                statusCode: 401,
+                body: JSON.stringify({
+                    message: "Unauthorized"
+                })
+            }
+        }
+
+        return {
+            statusCode: 200
+        }
+    } else {
+        console.log("Not a signup");
+        return {
+            statusCode: 200
+        };
+    }
+};
+
+module.exports = { handler };
